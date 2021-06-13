28c3e467f23eb995335c9210cb29e8af2b2f3566
Thiison <85842244+Thiison@users.noreply.github.com>
2021-06-13 21:56:21 +0200

    Add files via upload

---
+++ esx_atm/locales/en.lua
@@ -0,0 +1,7 @@
+Locales['en'] = {
+  ['invalid_amount'] = '~r~That\'s an invalid amount of money~s~',
+  ['deposit_money']  = 'Betegyél pénzt ~g~$%s~s~',
+  ['withdraw_money'] = 'visszavont ~g~$%s~s~',
+  ['press_e_atm']    = 'Nyomja meg ~INPUT_PICKUP~ Hogy megnyissa az atm-et ~g~ATM~s~',
+  ['atm_blip']       = 'Bank',
+}
